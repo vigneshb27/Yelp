3b3c196187e56e0221a593cba355a8886a2eac58
Vignesh_B <vigneshbaskaran2709@gmail.com>
2022-02-03 22:53:02 +0530

    BoilerPlate for the layout is added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -13,6 +14,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
