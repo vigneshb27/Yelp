e8d7fdbdd629d6f04f5b0eb166217cc78eb123f9
Vignesh_B <vigneshbaskaran2709@gmail.com>
2022-03-04 12:42:54 +0530

    Login routes

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,28 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', (req, res) => {
-    res.send(req.body);
-});
+router.post('/register', catchAsync(async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+})
+
 
 module.exports = router;
