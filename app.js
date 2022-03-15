7176da7553946da10dcdd400462c1320488543d6
Vignesh_B <vigneshbaskaran2709@gmail.com>
2022-03-15 23:23:47 +0530

    Uploading to cloudinary basics

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if(process.env.NODE_ENV != "production")
 {
     require('dotenv').config();
 }
-console.log(process.env.CLOUDINARY_KEY)
 
 const express = require('express');
 const app = express();
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,9 @@ const catchAsync = require('../utils/catchAsync');
 const campgrounds = require('../controllers/campgrounds');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const {storage} = require('../cloudinary')
+const upload = multer({ storage })
 
 const Campground = require('../models/campground');
 
