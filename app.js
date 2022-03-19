29ee8c04f50d0c3682226ce75c211c7606f423bc
Vignesh_B <vigneshbaskaran2709@gmail.com>
2022-03-19 13:44:18 +0530

    Fixed mongo injection

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -35,6 +36,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
