50a616592e5be9042dea602876edbf9f477e1d5c
Vignesh_B <vigneshbaskaran2709@gmail.com>
2022-04-24 11:45:35 +0530

    Updated cluster maps

--- public/javascripts/clusterMap/campground.js
+++ public/javascripts/clusterMap/campground.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+    center: [78.9629, 20.5937],
+    zoom: 4
 });
 
 map.addControl(new mapboxgl.NavigationControl());
